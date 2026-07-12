1c34b05c2b35298269d39aaf12746b86450ab3f7
Abhishek Pal <abhishekpalgbsss3@gmail.com>
2026-07-12 23:48:50 +0530

    Initail project setup with React, SpringBoot and PostgreSQL

---
+++ smart-interview-tracker-backend/src/main/java/com/abhishek/smart_interview_tracker_backend/SmartInterviewTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek.smart_interview_tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartInterviewTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartInterviewTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ smart-interview-tracker-backend/src/test/java/com/abhishek/smart_interview_tracker_backend/SmartInterviewTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.smart_interview_tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartInterviewTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
